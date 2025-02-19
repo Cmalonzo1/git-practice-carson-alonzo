21f068f4a953d6dff761b2c07bd0b056cdf8d95c
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2025-02-19 15:14:36 -0600

    Couple of changes
    - Renamed functions to reflect their functionality
    - Created docstrings for each function

--- Assignment-1/poweruserchallenge.py
+++ Assignment-1/poweruserchallenge.py
@@ -1,10 +1,21 @@
-def p(b):
-    for r in b:
-        print(" | ".join(r))
+def create_board(board):
+    """
+    Creates the gameboard
+    :param b: the board
+    :return:
+    """
+    for row in board:
+        print(" | ".join(row))
         print("-" * 5)
 
 
-def c_w(b, p):
+def check_winner(b, p):
+    """
+    Checks and returns true if the player place three marks in a row horizontally or vertically.
+    :param b: the board
+    :param p: player's piece
+    :return: true if the player scores 3 pieces in a row vertically or horizontally
+    """
     for i in range(3):
         if all(b[i][j] == p for j in range(3)) or all(b[j][i] == p for j in range(3)):
             return True
@@ -13,35 +24,47 @@ def c_w(b, p):
     return False
 
 
-def f(b):
+def check_if_board_full(b):
+    """
+    Runs a check on the board to make sure it's not full of pieces
+    :param b: the board
+    :return: all the spaces that have a piece in them
+    """
     return all(c != " " for r in b for c in r)
 
 
-def t():
-    b = [[" " for _ in range(3)] for _ in range(3)]
-    p = ["X", "O"]
+def play_game():
+
+    """
+    The main game loop
+    Players take turns placing their piece in a valid square
+    Runs checks to ensure the square exists
+    Game ends once a player scores 3 piece in a row vertically or horizontally
+
+    """
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    piece = ["X", "O"]
     print("Tic-Tac-Toe Game")
-    p(b)
+    create_board(board)
     for t in range(9):
-        pl = p[t % 2]
-        while 1:
+        player = piece[t % 2]
+        while True:
             try:
-                r, c = map(int, input(f"P {pl}, row col (0-2): ").split())
-                if b[r][c] == " ":
-                    b[r][c] = pl
+                row, col = map(int, input(f"P {player}, row col (0-2): ").split())
+                if board[row][col] == " ":
+                    board[row][col] = player
                     break
                 else:
                     print("Nope. Again.")
             except:
                 print("Wrong. 0-2 pls.")
-        p(b)
-        if c_w(b, pl):
-            print(f"P {pl} wins!")
+        create_board(board)
+        if check_winner(board, player):
+            print(f"P {player} wins!")
             return
-        if f(b):
+        if check_if_board_full(board):
             print("Draw!")
             return
     print("Draw!")
 
-
-t()
+play_game()

fab1742b8f977de39db51300520b20211b1b347b
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2025-02-19 18:38:57 -0600

    Few changes (Final Commit)
    New Function:
    
    - Extracted input handling & move validation from play_game() and created get_player_input()
    
    - Input handling now takes args (1-3) for better clarity on where the player's piece is being placed
    
    Updated the except condition checking player input tot throw a ValueError if the player's input is invalid
    
    Other:
    
    - Updated some of the docstrings to better clarify their functionality

--- Assignment-1/poweruserchallenge.py
+++ Assignment-1/poweruserchallenge.py
@@ -1,32 +1,49 @@
 def create_board(board):
     """
     Creates the gameboard
-    :param b: the board
-    :return:
+    :param board: the board
     """
     for row in board:
         print(" | ".join(row))
-        print("-" * 5)
+        print("- " * 5)
 
-
-def check_winner(b, p):
+def get_player_input(board, player):
     """
-    Checks and returns true if the player place three marks in a row horizontally or vertically.
-    :param b: the board
-    :param p: player's piece
-    :return: true if the player scores 3 pieces in a row vertically or horizontally
+    Handles user input, checks if the move is valid and, if valid, checks if a piece is not already in the selected
+    slot
+
+    :param board: The board
+    :param player: the player making the move
+    """
+    while True:
+        try:
+            row, col = map(int, input(f"P {player}, row col (1-3): ").split())
+            if board[row - 1][col - 1] == " ":
+                board[row - 1][col - 1] = player
+                break
+            else:
+                print("Nope. Again.")
+        except ValueError:
+            print("Wrong. 0-2 pls.")
+
+def check_winner(board, piece):
+    """
+    Checks and returns true if the player place three pieces in a row, column, or diagonal.
+    :param board: the board
+    :param piece: the player's piece
+    :return: true if the player scores 3 pieces in a row, column, or diagonal
     """
     for i in range(3):
-        if all(b[i][j] == p for j in range(3)) or all(b[j][i] == p for j in range(3)):
+        if all(board[i][j] == piece for j in range(3)) or all(board[j][i] == piece for j in range(3)):
             return True
-    if all(b[i][i] == p for i in range(3)) or all(b[i][2 - i] == p for i in range(3)):
+    if all(board[i][i] == piece for i in range(3)) or all(board[i][2 - i] == piece for i in range(3)):
         return True
     return False
 
 
 def check_if_board_full(b):
     """
-    Runs a check on the board to make sure it's not full of pieces
+    Checks if the board is completely filled
     :param b: the board
     :return: all the spaces that have a piece in them
     """
@@ -37,10 +54,7 @@ def play_game():
 
     """
     The main game loop
-    Players take turns placing their piece in a valid square
-    Runs checks to ensure the square exists
-    Game ends once a player scores 3 piece in a row vertically or horizontally
-
+    :return: ends the game after the conditions are met
     """
     board = [[" " for _ in range(3)] for _ in range(3)]
     piece = ["X", "O"]
@@ -48,16 +62,9 @@ def play_game():
     create_board(board)
     for t in range(9):
         player = piece[t % 2]
-        while True:
-            try:
-                row, col = map(int, input(f"P {player}, row col (0-2): ").split())
-                if board[row][col] == " ":
-                    board[row][col] = player
-                    break
-                else:
-                    print("Nope. Again.")
-            except:
-                print("Wrong. 0-2 pls.")
+
+        get_player_input(board, player)
+
         create_board(board)
         if check_winner(board, player):
             print(f"P {player} wins!")
